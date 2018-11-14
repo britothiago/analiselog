f7750972c42857e0aee6f3ec8a6f392be8d3eb0e
britothiago <britotr@gmail.com>
2018-11-14 11:36:06 -0200

    Projeto Revisado

--- analiselog.py
+++ analiselog.py
@@ -15,9 +15,15 @@ def fechar_banco(conexao):
 def artigos_populares():
     pg = abrir_banco()
     c = pg.cursor()
-    c.execute("SELECT articles.title, count(log.path) AS total FROM log INNER JOIN \
-              articles ON log.path = '/article/' || articles.slug GROUP BY \
-              articles.title ORDER BY total DESC LIMIT 3")
+    c.execute("""
+                SELECT articles.title, count(log.path) AS total
+                FROM log
+                INNER JOIN articles ON
+                    log.path = '/article/' || articles.slug 
+                GROUP BY articles.title
+                ORDER BY total 
+                DESC LIMIT 3
+             """)
     artigos = c.fetchall()
     print('Artigos mais populares')
     for art in artigos:
@@ -28,10 +34,16 @@ def artigos_populares():
 def autores_populares():
     pg = abrir_banco()
     c = pg.cursor()
-    c.execute("SELECT authors.name, count(log.path) AS total FROM authors\
-                JOIN articles ON authors.id = articles.author\
-                JOIN log ON log.path = '/article/' || articles.slug\
-                GROUP BY authors.name ORDER BY total DESC")
+    c.execute("""
+                SELECT authors.name, count(log.path) AS total
+                FROM authors
+                JOIN articles 
+                    ON authors.id = articles.author
+                JOIN log 
+                    ON log.path = '/article/' || articles.slug
+                GROUP BY authors.name
+                ORDER BY total DESC
+             """)
     autor = c.fetchall()
     print('Autores mais visualizados')
     for aut in autor:
@@ -43,19 +55,37 @@ def autores_populares():
 def porcentagem():
     pg = abrir_banco()
     c = pg.cursor()
-    c.execute("CREATE VIEW erro AS SELECT to_char(time, 'Mon DD, YYYY') as data, \
-                count(time) as totalerro \
-                FROM log WHERE status LIKE '404%'\
-                GROUP BY data ORDER BY data ASC")
+    c.execute("""
+                CREATE VIEW erro AS 
+                SELECT 
+                    to_char(time, 'Mon DD, YYYY') AS data,
+                    COUNT(time) AS totalerro
+                FROM log
+                WHERE status 
+                    LIKE '404%'
+                GROUP BY data 
+                ORDER BY data ASC
+             """)
 
-    c.execute("CREATE VIEW nerro AS SELECT to_char(time, 'Mon DD, YYYY') as data, \
-                count(time) as totalnerro \
-                FROM log GROUP BY data ORDER BY data ASC")
+    c.execute("""
+                CREATE VIEW nerro AS
+                SELECT 
+                    to_char(time, 'Mon DD, YYYY') AS data,
+                    COUNT(time) AS totalnerro
+                FROM log 
+                GROUP BY data 
+                ORDER BY data ASC
+             """)
 
-    c.execute("SELECT erro.data, ROUND(100.0 * erro.totalerro / nerro.totalnerro, 2)\
-                FROM erro JOIN nerro ON nerro.data = erro.data\
-                WHERE ROUND(100.0 * erro.totalerro / nerro.totalnerro, 2) > 1\
-                ORDER BY ROUND DESC")
+    c.execute("""
+                SELECT erro.data, 
+                    ROUND(100.0 * erro.totalerro / nerro.totalnerro, 2)
+                FROM erro 
+                JOIN nerro 
+                    ON nerro.data = erro.data
+                WHERE ROUND(100.0 * erro.totalerro / nerro.totalnerro, 2) > 1
+                ORDER BY ROUND DESC
+             """)
     result = c.fetchall()
     print('Dia com percentual de mais erros de acesso')
     for final in result:
